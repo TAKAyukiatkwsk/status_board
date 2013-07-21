d0a39b736505193a25d6005f2ebf2cbb2aa77aeb
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2013-07-21 18:20:36 +0900

    Use JSONP

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 (function() {
-  var githubStatusAPI = 'https://status.github.com/api.json';
-  $.get(githubStatusAPI, function(data) {
-    var url = data;
+  var githubStatusAPI = 'https://status.github.com/api.json?callback=?';
+  $.getJSON(githubStatusAPI).done(function(data) {
+    console.log(data);
   });
 })();
