01c86d35043b7c863982ddefb22e3af79a9b001d
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2013-07-21 18:35:35 +0900

    Show GitHub status

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 (function() {
-  var githubStatusAPI = 'https://status.github.com/api.json?callback=?';
-  $.getJSON(githubStatusAPI).done(function(data) {
-    console.log(data);
+  var githubStatusAPI = {
+    'api': 'https://status.github.com/api.json?callback=?'
+  };
+  $.getJSON(githubStatusAPI.api).done(function(data) {
+    githubStatusAPI.status_url = data.status_url + '?callback=?';
+    $.getJSON(githubStatusAPI.status_url).done(function(data) {
+      $('#status_list span.status').html(data.status);
+    });
   });
 })();
